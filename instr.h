6437fe5eae738c075eb9ee6de0b0f92ba7b16cdd
yurickyh <yurickyussuke@hotmail.com>
2017-10-12 01:21:48 -0300

    Criação da instrução ATR e adição de atributos de células no operando

---
+++ instr.h
@@ -0,0 +1,71 @@
+/* Códigos das instruções */
+
+//Operando
+typedef enum {
+  PUSH,
+  POP,
+  DUP,
+  ADD,
+  SUB,
+  MUL,
+  DIV,
+  JMP,
+  JIT,
+  JIF,
+  CALL,
+  RET,
+  EQ,
+  GT,
+  GE,
+  LT,
+  LE,
+  NE,
+  STO,
+  RCL,
+  END,
+  PRN,
+  STL,
+  RCE,
+  ALC,
+  FRE,
+  ATR//,
+  //MOVE,
+  //RECO,
+  //DEPO,
+  //ATAQ
+} OpCode;
+
+/* Tipos dos operandos */
+/* no momento, são todos inteiros */
+typedef enum {
+  NUM,
+  ACAO,
+  VAR,
+  CEL,
+  TER
+} Tipo;
+
+typedef struct {
+   Tipo t;
+   int val;
+   union{
+    struct {
+      int terrain;
+      int cristal;
+      int ocup;
+      int baseColour;
+      //Base base;
+    } CEL;
+   } Controlador;
+   /*union {
+   int n;
+   int ac;
+   int v;
+   } val;*/ 
+} OPERANDO;
+
+/* Instrução */
+typedef struct {
+  OpCode instr;
+  OPERANDO op;
+} INSTR;
