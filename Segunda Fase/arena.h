da79d90d7e9cc10370e69dd57518fba9b23e1cab
kaiquekk <32721660+kaiquekk@users.noreply.github.com>
2017-10-18 21:02:31 -0200

    Add files via upload

--- Segunda Fase/arena.h
+++ Segunda Fase/arena.h
@@ -6,48 +6,49 @@
 #define MAXMATRIZL 4
 #define MAXMATRIZC 8
 #define BASELIFE 20
+#define INSTRNUMBER 50
 //Ao mudar o valor de MAXMAQ ou MAXEXERC ou ROBOSONEXERC, e preciso acertar os valores dos outros de modo que MAXMAQ=MAXEXERC*ROBOSONEXERC
 
 typedef enum{
-    road,
-    mountain,
-    river
-    //Adicionar mais depois
+	road,
+	mountain,
+	river
+	//Adicionar mais depois
 } Terreno;
 
 //Colour = 0 -> não é base
 //Colour = outro numero -> é base
 typedef struct{
-    int vida;
-    int colour; //Identificação da base	
+	int vida;
+	int colour; //Identificação da base	
 } Base;
 
 typedef struct{
-    Terreno terrain;
-    int cristal;
-    int ocup;//0 = desocupada; 1 ate MAXMAQ = indice do robo no vetor de endereços de maquina da arena; MAXMAQ+1 = base;
-    int baseColour;
+	Terreno terrain;
+	int cristal;
+	int ocup;//0 = desocupada; 1 ate MAXMAQ = indice do robo no vetor de endereços de maquina da arena; MAXMAQ+1 = base;
+	int baseColour;
 } Celula;
 
 typedef struct{
-    Maquina* robots[ROBOSONEXERC];
-    Base* base;
+	Maquina* robots[ROBOSONEXERC];
+	Base* base;
 } Exercito;
 
 typedef struct{
-    Maquina* robos[MAXMAQ];
-    int robosTopo;
-    Celula matriz[MAXMATRIZL][MAXMATRIZC];
-    Exercito* exerc[MAXEXERC];
-    int exercTopo;	
-    int baseCount[MAXEXERC+1];
+	Maquina* robos[MAXMAQ];
+	int robosTopo;
+	Celula matriz[MAXMATRIZL][MAXMATRIZC];
+	Exercito* exerc[MAXEXERC];
+	int exercTopo;	
+	int baseCount[MAXEXERC+1];
 } Arena;
 
 extern Arena *a;
 
 typedef struct{
-    int x;
-    int y;
+	int x;
+	int y;
 } Coord;
 
 void CriaArena();
--- Segunda Fase/maq.h
+++ Segunda Fase/maq.h
@@ -3,16 +3,16 @@
 #define MAXMEM 100
 
 typedef struct {
-    Pilha pil;
-    Pilha exec;
-    OPERANDO Mem[MAXMEM];
-    INSTR *prog;
-    int ip;
-    int rbp;
-    int position[2];
-    int cristal;
-    int vida;//se há ataque, faz sentido o rebo ter uma barra de vida
-    int index;//index no vetor de endereços da arena
+  Pilha pil;
+  Pilha exec;
+  OPERANDO Mem[MAXMEM];
+  INSTR *prog;
+  int ip;
+  int rbp;
+  int position[2];
+  int cristal;
+  int vida;//se há ataque, faz sentido o rebo ter uma barra de vida
+  int index;//index no vetor de endereços da arena
 } Maquina;
 
 Maquina *cria_maquina(INSTR *p);
