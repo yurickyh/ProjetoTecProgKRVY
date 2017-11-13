894369acb9a0d5a9454745e8448b952d858d2e5e
kaiquekk <32721660+kaiquekk@users.noreply.github.com>
2017-11-13 19:59:38 -0200

    Update motor.c

--- Terceira Fase/motor.c
+++ Terceira Fase/motor.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include "arena.h"
 #include <string.h>
-#define linhas 4
-#define colunas 8
 
 FILE *display;
 INSTR programa[] = {
-    //FATORIAL. Para numeros muito grandes é preciso aumentar o valor da variavel INSTRNUMBER ou aumentar o numero do argumento passado na funcao Atualiza():
-    //Descomentar para o teste 6.
-    /*{PUSH, {NUM,  6}}, //0
+    /*FATORIAL:
+    {PUSH, {NUM,  6}}, //0
     {CALL, {NUM,  4}}, //1
     {PRN,  {NUM,  0}}, //2
     {END,  {NUM,  0}}, //3
@@ -29,37 +26,60 @@ INSTR programa[] = {
     {MUL,  {NUM,  0}}, //18
     {FRE,  {NUM,  1}}, //19
     {RET,  {NUM,  0}}  //20*/
-     
-    //Descomentar para o teste 7
-    /*{MOVE, {ACAO, 315}},
-    {MOVE, {ACAO, 270}},
-    {MOVE, {ACAO,  45}}*/
-
-    //Descomentar para o teste 8
-    /*{RECO, {ACAO, 315}},
-    {MOVE, {ACAO, 270}},
-    {RECO, {ACAO, 270}}*/
-
-    //Descomentar para o teste 9
-    /*{RECO, {ACAO, 315}},
-    {MOVE, {ACAO, 270}},
-    {DEPO, {ACAO,  45}}*/
-
-    //Descomentar para o teste 10
-    /*{MOVE, {ACAO, 315}},
-    {MOVE, {ACAO, 270}},
-    {ATAQ, {ACAO, 270}}*/
-
-    //Descomentar para o teste 11
-    /*{PUSH, {CEL, 1,{river, 2, 10, 6}}},
-    {ATR, {NUM, 0}},
-    {PRN, {NUM, 0}},
-    {END,  {NUM,  0}}*/
 };
 
 int main(int ac, char **av) {
 
-    int lin = 0, col = 0, color = 0;
+    CriaArena();
+    display = popen("java Map > map.txt", "r");
+    pclose(display);
+    char s[1024];
+    char * pch;
+    int i=0, j=0, count=0;
+    display = fopen("map.txt", "r");
+    fgets(s, 1024, file);
+    pch = strtok(s, " ");
+    while(pch!=NULL)
+    {
+        if(count==0)
+        {
+            a->matriz[i][j].terrain = pch;]
+            count++;
+        }
+        else if(count==1)
+        {
+            a->matriz[i][j].cristal = (int)pch;
+            count++;        
+        }
+        else if(count==2)
+        {
+            a->matriz[i][j].ocup = (int)pch;
+            count++;        
+        }
+        else if(count==3)
+        {
+            a->matriz[i][j].baseCoulour = (int)pch;
+            count = 0;
+            if(j+1==MAXMATRIZC)
+            {
+                i++;
+                j=0;
+            }
+            else
+            {
+                j++;            
+            }        
+        }
+        if(i==MAXMATRIZL)
+        {
+            break;        
+        }
+        pch = strtok(NULL, " ");
+    }
+    fclose(file);
+    printf("Teste %2d\n", a->matriz[2][2].cristal);
+    
+    /*int lin = 0, col = 0, color = 0;
     char *TER[] = {
         "road",
         "mountain",
@@ -71,7 +91,7 @@ int main(int ac, char **av) {
         "30 144 255"//azul = river
     };
     CriaArena();
-    Celula c[linhas][colunas] = {
+    Celula c[MAXMATRIZL][MAXMATRIZC] = {
         {{road, 0, 0, 1}, {road, 2, 0, 0}, {road, 2, 0, 0}, {road, 0, 0, 0}, {river, 3, 0, 0}, {mountain, 4, 0, 0}, {road, 2, 0, 0}, {road, 2, 0, 0}}, 
         {{river, 4, 0, 0}, {river, 3, 0, 0}, {mountain, 2, 0, 0}, {road, 2, 0, 0}, {road, 1, 0, 0}, {road, 1, 0, 0}, {river, 0, 0, 0}, {river, 1, 0, 0}},
         {{river, 2, 0, 0}, {river, 2, 0, 0}, {river, 2, 0, 0}, {road, 2, 0, 0}, {road, 2, 0, 0}, {road, 2, 0, 0}, {mountain, 2, 0, 0}, {mountain, 3, 0, 0}},
@@ -83,8 +103,8 @@ int main(int ac, char **av) {
         fprintf(stderr,"Não encontrei o programa de exibição\n");
         return 1;
     }  
-    for (lin = 0; lin < linhas; lin++){
-        for (col = 0; col < colunas; col++){
+    for (lin = 0; lin < MAXMATRIZL; lin++){
+        for (col = 0; col < MAXMATRIZC; col++){
             if(a->matriz[lin][col].baseColour == 1){
                 printf("\nEntrou aqui");
                 fprintf(display, "base base1r.png %d %d %d\n", a->matriz[lin][col].baseColour, lin, col); //criar a base vermelha            
@@ -99,7 +119,7 @@ int main(int ac, char **av) {
             fflush(display);   
         }
     }
-    pclose(display);
+    pclose(display);*/
 
     return 0;
 }
